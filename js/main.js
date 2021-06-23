536c83d6de6e0833ef7cc429a702fb9e2b8f2b31
ca1r0n <egorfrolov2019.ef@gmail.com>
2021-06-23 11:21:14 +0300

    end

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var burger = document.querySelector('.burger');
+burger.addEventListener('click', function (e) {
+  e.currentTarget.classList.toggle('burger--active');
+});
+//# sourceMappingURL=main.js.map
